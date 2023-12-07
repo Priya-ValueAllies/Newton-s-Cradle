73576efa6ec287b1e165cec7ab4c55f99358e8b1
Priya-ValueAllies <153093543+Priya-ValueAllies@users.noreply.github.com>
2023-12-07 12:04:07 +0530

    Update sketch.js
    added few more strings

--- sketch.js
+++ sketch.js
@@ -9,22 +9,28 @@ function setup(){
 	createCanvas(400,400)
 	engine = Engine.create()
 	world = engine.world
-	
-	rod1 = new Rod(50,50,150,20)
-	ball1 = new Ball(100,100,20)
-	rope1 = new Rope(ball1.body,{x:100,y:60})
-	ball2 = new Ball(120,100,20)
-	rope2 = new Rope(ball2.body,{x:120,y:60})
-	ball3 = new Ball(140,100,20)
-	rope3 = new Rope(ball3.body,{x:140,y:60})
+	rod1 = new Rod(50,100,300,20)
+	ball1 = new Ball(100,300,20)
+	rope1 = new Rope(ball1.body,{x:100,y:100})
+	ball2 = new Ball(140,300,20)
+	rope2 = new Rope(ball2.body,{x:140,y:100})
+	ball3 = new Ball(180,300,20)
+	rope3 = new Rope(ball3.body,{x:180,y:100})
+	ball4 = new Ball(220,300,20)
+	rope4 = new Rope(ball4.body,{x:220,y:100})
+	ball5 = new Ball(260,300,20)
+	rope5 = new Rope(ball5.body,{x:260,y:100})
 	Engine.run(engine)
 }
 
 
 function draw(){
-	background(255,0,0)
+	background("pink")
+	textSize(20)
+	fill("purple")
 	text("NEWTON's CRADLE",100,20)
-	text("PRESS UP AROW TO SEE THE CRADLE's MOVEMENT",50,50);
+	textSize(15)
+	text("PRESS UP AROW TO SEE THE CRADLE's MOVEMENT",10,50);
 rod1.display()
 ball1.show()
 rope1.display()
@@ -32,6 +38,10 @@ ball2.show()
 rope2.display()
 ball3.show()
 rope3.display()
+ball4.show()
+rope4.display()
+ball5.show()
+rope5.display()
 }
 
 
